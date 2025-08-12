8116fbe171e6fa37a0a32467f1e39cd8daa2dcce
Vincent <magicdroidx@gmail.com>
2025-08-12 18:08:53 +0100

    feat: add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,79 @@
-import Image from "next/image";
+import {AnimatedGridPattern} from "@/components/magicui/animated-grid-pattern";
+import {cn} from "@/lib/utils";
+import {AnimatedSpan, Terminal, TypingAnimation} from "@/components/magicui/terminal";
+import Link from "next/link";
+import {ChevronRight} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import {FaGithub} from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      <div className="relative isolate px-6 lg:px-8">
+    <div>
+      <section id="hero" className="relative h-full">
+        <AnimatedGridPattern maxOpacity={0.05}
+                             numSquares={2000}
+                             width={200}
+                             height={200}
+                             duration={3}
+                             className={cn(
+                               "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+                               "inset-x-0 h-[100%]",
+                             )}/>
         <div
-          aria-hidden="true"
-          className="absolute inset-x-0 -top-80 -z-10 transform-gpu overflow-hidden blur-3xl"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%-30rem)] w-[72.1875rem] aspect-1155/678 -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-          />
-        </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div
-              className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span aria-hidden="true" className="absolute inset-0"/>
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
-          <div className="text-center">
-            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Data to enrich your online business
+          className="relative h-full overflow-hidden mx-auto mx-auto max-w-(--nextra-content-width) pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)] py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:py-40">
+          <div className="mx-auto max-w-4xl lg:mx-0 lg:flex-auto">
+            <h1 className="mt-10 text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-6xl">
+              Full-featured Server Software for Minecraft: Bedrock Edition
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat.
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque
+              faucibus.
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
+            <div className="mt-10 flex items-center gap-x-4">
+              <Button className="rounded-xl" asChild>
+                <Link href="/docs/getting-started">
+                  Getting Started
+                  <ChevronRight
+                    className="size-4 shrink-0 transition-all duration-300 ease-out group-hover:translate-x-1"/>
+                </Link>
+              </Button>
+              <Button className="rounded-xl" variant="outline" asChild>
+                <Link href="https://github.com/EndstoneMC/endstone">
+                  <FaGithub
+                    className="size-4 shrink-0 transition-all duration-300 ease-out group-hover:translate-x-1"/>
+                  Star on GitHub
+                </Link>
+              </Button>
             </div>
           </div>
+          <div className="mt-16 lg:mt-10 items-center justify-center flex">
+            <Terminal className="h-[18rem]">
+              <TypingAnimation>&gt; pipx run endstone</TypingAnimation>
+              <AnimatedSpan>[Server] Starting Server</AnimatedSpan>
+              <AnimatedSpan>[Server] Level Name: Bedrock level</AnimatedSpan>
+              <AnimatedSpan>[Database] Opening level &#39;worlds/Bedrock level/db&#39;</AnimatedSpan>
+              <AnimatedSpan>[Network] IPv4 supported, port: 19132: Used for gameplay and LAN discovery</AnimatedSpan>
+              <AnimatedSpan>[Network] IPv6 supported, port: 19133: Used for gameplay</AnimatedSpan>
+              <AnimatedSpan className="text-green-500">
+                [Server] Server started.
+              </AnimatedSpan>
+              <TypingAnimation>&gt;</TypingAnimation>
+            </Terminal>
+          </div>
         </div>
+      </section>
+      <section id="showcase" className="relative py-14 bg-gray-200 dark:bg-gray-800">
         <div
-          aria-hidden="true"
-          className="absolute inset-x-0 top-50 sm:top-20 -z-10 transform-gpu overflow-hidden blur-2xl"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          />
+          className="flex flex-col w-full max-w-full gap-4 py-1 px-7 md:px-10 md:mx-auto mx-auto max-w-(--nextra-content-width) pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+          <h2 className="mb-2 text-center text-5xl font-bold leading-[1.2] tracking-tighter text-foreground">
+            Why Endstone?
+          </h2>
+          <h3
+            className="mx-auto mb-8 text-balance text-center text-lg font-medium tracking-tight text-foreground/80">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit.
+          </h3>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
