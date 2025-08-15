52c5e4f9ebce425fdc9501a51d67f5df81de3110
Vincent <magicdroidx@gmail.com>
2025-08-16 00:23:12 +0100

    feat: add script for Doxygen integration with nextra

---
+++ scripts/mkdoxy/generator.py
@@ -0,0 +1,23 @@
+import os
+
+from mkdocs.structure import files
+from mkdoxy.generatorAuto import GeneratorAuto
+from mkdoxy.node import Node
+import inflection
+
+
+class Generator(GeneratorAuto):
+    def save(self, path: str, output: str):
+        rel_path = os.path.join(self.apiPath, path)
+        self.fullDocFiles.append(files.File(rel_path, self.tempDoxyDir, self.siteDir, self.useDirectoryUrls))
+        with open(os.path.join(self.siteDir, rel_path), "w", encoding="utf-8") as file:
+            file.write(output)
+
+    def member(self, node: Node, config: dict = None):
+        path = inflection.underscore(node.name_short) + ".md"
+
+        output = self.generatorBase.member(node, config)
+        self.save(path, output)
+
+        if node.is_language or node.is_group or node.is_file or node.is_dir:
+            self.members(node.children, config)
---
+++ scripts/mkdoxy/main.py
@@ -0,0 +1,81 @@
+import argparse
+import logging
+from pathlib import Path
+
+from mkdoxy.cache import Cache
+from mkdoxy.constants import Kind
+from mkdoxy.doxygen import Doxygen
+from mkdoxy.doxyrun import DoxygenRun
+from mkdoxy.generatorBase import GeneratorBase
+from mkdoxy.xml_parser import XmlParser
+from generator import Generator
+
+logging.basicConfig()
+log: logging.Logger = logging.getLogger("mkdocs")
+log.setLevel(logging.DEBUG)
+
+
+def main(base_dir: Path, debug: bool = True):
+    temp_dir = Path(__file__).parent.parent.parent / ".doxygen"
+    temp_dir.mkdir(parents=True, exist_ok=True)
+
+    site_dir = Path(__file__).parent.parent.parent / "content"
+    site_dir.mkdir(parents=True, exist_ok=True)
+
+    config = {
+        "FILE_PATTERNS": "*.h",
+        "EXCLUDE_PATTERNS": "*/detail/*",
+        "RECURSIVE": True,
+        "STRIP_FROM_INC_PATH": "include/",
+    }
+
+    runner = DoxygenRun('doxygen', str(base_dir / "include" / "endstone"), str(temp_dir), config, "")
+    if runner.checkAndRun():
+        log.info("  -> generating Doxygen files")
+    else:
+        log.info("  -> skip generating Doxygen files (nothing changes)")
+
+    # Parse XML to basic structure
+    cache = Cache()
+    parser = XmlParser(cache=cache, debug=debug)
+
+    # Parse basic structure to recursive Nodes
+    doxygen = Doxygen(str(runner.getOutputFolder()), parser=parser, cache=cache)
+
+    # Print parsed files
+    # if debug:
+    #     doxygen.printStructure()
+
+    # Prepare generator for future use (GeneratorAuto, SnippetGenerator)
+    base_generator = GeneratorBase("", ignore_errors=False, debug=debug)
+
+    generator = Generator(
+        generatorBase=base_generator,
+        tempDoxyDir=str(temp_dir),
+        siteDir=str(site_dir),
+        apiPath="reference/cpp",
+        doxygen=doxygen,
+        useDirectoryUrls=True,
+    )
+
+    template_config = {}
+    root_node = None
+    for node in generator.doxygen.root.children:
+        if node.kind == Kind.NAMESPACE and node.refid == "namespaceendstone":
+            root_node = node
+            break
+
+    assert root_node is not None, "root node not found"
+    nodes = [node for node in root_node.children if node.kind == Kind.CLASS]
+    generator.members(nodes, template_config)
+    generator.classes(nodes, template_config)
+
+    for file in generator.fullDocFiles:
+        log.info(file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("base_dir", type=Path)
+    args = parser.parse_args()
+    main(args.base_dir, debug=True)
