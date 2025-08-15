9b59ea7824e4a4cf7258837e18cb9cd199c890c1
Vincent <magicdroidx@gmail.com>
2025-08-15 13:40:45 +0100

    feat: add doc symbol

--- scripts/griffe2mdx/rendering_override.py
+++ scripts/griffe2mdx/rendering_override.py
@@ -1,13 +1,20 @@
-def do_heading(content: str, heading_level: int, **attributes: str) -> str:
+def do_heading(content: str, heading_level: int, role: str | None = None, **attributes: str) -> str:
     """Render a Markdown heading.
 
     Arguments:
         content: The HTML within the heading.
         heading_level: The level of heading (e.g. 3 -> `###`).
+        role: An optional role for the object bound to this heading.
         **attributes: Any extra attributes of the heading.
     """
 
-    heading = f"\n{'#' * heading_level} {content}"
+    heading = f"\n{'#' * heading_level}"
+
+    if role:
+        heading += f' <code className="doc-symbol doc-symbol-{role}"/>'
+
+    heading += f" {content}"
+
     if "id" in attributes:
         heading += f" [#{attributes['id']}]"
 
